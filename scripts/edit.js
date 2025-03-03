bc8747b5af29eea2870885a104ab80d2408c4635
sethadenoIVYT <156937009+sethadenoIVYT@users.noreply.github.com>
2025-03-03 13:18:25 -0500

    Update edit.js

--- scripts/edit.js
+++ scripts/edit.js
@@ -2,7 +2,7 @@ addEventListener("DOMContentLoaded", async function(){
     document.querySelector("#updateBtn").addEventListener("click",updateSong)
     const urlparam = new URLSearchParams(window.location.search)
     const songID = urlparam.get('id')
-    const response = await fetch("http://localhost:3000/api/songs" + songID)
+    const response = await fetch("https://cyclic-awake-report.glitch.me/api/songs" + songID)
     if(response.ok){
         let song = await response.json()
         document.querySelector("#title").value = song.title,
@@ -27,7 +27,7 @@ async function updateSong() {
     };
          
      // Send PUT request with JSON-encoded song to Music API
-    const response = await fetch("http://localhost:3000/api/songs" + songID, {
+    const response = await fetch("https://cyclic-awake-report.glitch.me/api/songs" + songID, {
        method: "PUT",
        headers: {
           "Content-Type": "application/json"
