d921ced88426253109a66c52f669d87f9b4bf273
sethadenoIVYT <156937009+sethadenoIVYT@users.noreply.github.com>
2025-03-03 13:18:03 -0500

    Update details.js

--- scripts/details.js
+++ scripts/details.js
@@ -4,7 +4,7 @@ addEventListener("DOMContentLoaded", async function() {
     const songID = urlparam.get('id')
     console.log(songID)
 
-    const response = await fetch("http://localhost:3000/api/songs" + songID)
+    const response = await fetch("https://cyclic-awake-report.glitch.me/api/songs" + songID)
     const song = await response.json()
     console.log(song)
 
