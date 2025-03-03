ca4c30649f1bd8458e26ef5d8580044aafb49978
Seth Deno <sdeno3@ivytech.edu>
2025-03-03 12:53:50 -0500

    add front end files for mod7 tutorial

---
+++ scripts/delete.js
@@ -0,0 +1,32 @@
+addEventListener("DOMContentLoaded", async function() {
+    document.querySelector("#deleteBtn").addEventListener("click", deleteSong)
+    getAllSongs()
+})
+
+async function getAllSongs() {
+    const response = await fetch("http://localhost:3000/api/songs")
+    if (response.ok){
+        const songs = await response.json()
+        let html = ""
+        for (let song of songs){
+            html += `<option value="${song._id}">${song.title}</option>`
+        }
+        document.querySelector("#songDropDown").innerHTML = html
+    }
+    
+}
+
+async function deleteSong(){
+    //grab id of selected song from the dropdown
+    const songID = document.querySelector("#songDropDown option:checked").value
+    const response = await fetch("http://localhost:3000/api/songs/" + songID, {
+        method: "DELETE"
+    })
+    if(response.ok){
+        alert("Song deleted")
+        getAllSongs()        
+    }
+    else{
+        document.querySelector("#error").innerHTML = "Cannot delete song"
+    }
+}
---
+++ scripts/login.js
@@ -0,0 +1,44 @@
+let token
+
+window.onload=function(){
+document.querySelector("#loginBtn").addEventListener("click",function() {
+    const username = document.querySelector("#username").value
+    const password = document.querySelector("#password").value
+
+    login(username, password)
+})}
+
+async function login(username, password) {
+    const login_cred = {
+        username,
+        password
+    }
+
+    const response = await fetch("http://localhost:3000/api/auth/", {
+        method: "POST",
+        headers:{
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify(login_cred)
+
+    })
+
+    if(response.ok) {
+        const tokenResponse = await response.json()
+        token = tokenResponse.token
+        uname = tokenResponse.username2
+        auth = tokenResponse.auth
+
+        console.log(token)
+
+        localStorage.setItem("token", token)
+        localStorage.setItem("uname", uname)
+        localStorage.setItem("auth", auth)
+
+        window.location.replace("/index.html")
+    }
+    else {
+        document.querySelector("errorMsg").innerHTML = "Bad username and Password"
+    }
+
+}
